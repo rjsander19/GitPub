ba66ab1976d72dfe73b31be6a646a396670909b5
rjsander <rjsander19@gmail.com>
2023-06-13 16:41:44 -0700

    adds models

---
+++ models/drinks.js
@@ -0,0 +1,45 @@
+
+const drinks = [
+    {
+      name: "Cruddy Mary",
+      price: 132,
+      image: "https://i.imgur.com/Va5iIw5",
+    },
+    {
+      name: "Index On The Beach",
+      price: 68,
+      image: "https://i.imgur.com/XV2aPa2",
+    },
+    {
+      name: "Hack & Coke",
+      price: 1,
+      image: "https://i.imgur.com/rLOXFRI",
+    },
+    {
+      name: "Whiskey-Value Pair",
+      price: 11,
+      image: "https://i.imgur.com/a7rmkoS",
+    },
+    {
+      name: "404 horsemen",
+      price: 202,
+      image: "https://i.imgur.com/FLucOLr",
+    },
+    {
+      name: "If StateMint Julep",
+      price: 2,
+      image: "https://i.imgur.com/yaoK0Dg",
+    },
+    {
+      name: "APIPA",
+      price: 43,
+      image: "https://i.imgur.com/qAhA7pi",
+    },
+    {
+      name: "Node To Joy",
+      price: 56,
+      image: "https://i.imgur.com/MbVdwZz",
+    },
+  ]
+
+  module.exports = drinks
